b4d67b6a84c252e84ce1a5f4502f604382cd7bc2
Julio Orellana <julio.orellana@ideauno.cl>
2017-07-19 23:59:05 -0400

    basic files added

--- app/index.ios.js
+++ app/index.ios.js
@@ -1,4 +1,5 @@
 
 import { AppRegistry } from 'react-native';
+import app from './src/app'
 
 AppRegistry.registerComponent('app', () => app);
---
+++ app/src/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux'
+// import * as testReducer from './test'
+
+
+export default combineReducers(Object.assign(
+    //testReducer, ...
+))
